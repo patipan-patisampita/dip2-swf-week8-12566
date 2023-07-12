966542f4383a4085fa659ba9a81491d487c4aa18
patipan-patisampita <patipan-patisampita@gmail.com>
2023-07-12 15:17:32 +0700

    update

--- resources/views/pages/employee/index.blade.php
+++ resources/views/pages/employee/index.blade.php
@@ -1,7 +1,49 @@
 @extends('layouts.frontend')
 
 @section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 mt-4 text-center">
 
-    <h1>Fetch data from Employee table</h1>
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="card-title">Fetch Data Database using Eloquent Model</h5>
+                    </div>
+                    <div class="card-body">
 
+                    </div>
+                </div>
+
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection
